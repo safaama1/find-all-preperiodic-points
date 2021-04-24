b2b309d8061aa432e63bd43e25d482d7e555f01a
Safa Amasha <61747440+safaama1@users.noreply.github.com>
2021-04-24 17:31:30 +0300

    Update rational_preperiodic_graph.py

--- rational_preperiodic_graph.py
+++ rational_preperiodic_graph.py
@@ -1,8 +1,8 @@
 def rational_preperiodic_graph(self,primes,e):
     DS = copy(self)
     periods = DS.all_possible_global_periods(primes,e)
-    periodic_points_ = DS.all_periodic_points(periods)
-    preperiodic_points_ = DS.all_preperiodic_points(periodic_points_)
+    periodic_points_ = DS._all_periodic_points_(periods)
+    preperiodic_points_ = DS._all_preperiodic_points_(periodic_points_)
     g1 = DS._preperiodic_points_to_cyclegraph(preperiodic_point_)
     # number every vertice on the graph
     g2 = g1.canonical_label()
