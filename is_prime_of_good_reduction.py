833c7a9ed03a95ad093161263914c9a5a73ff86b
Safa Amasha <safaamasha99@gmail.com>
2021-05-17 16:02:45 +0300

    Final Commit

--- is_prime_of_good_reduction.py
+++ is_prime_of_good_reduction.py
@@ -1,17 +1,11 @@
+# %load is_prime_of_good_reduction.py
 # Function to check if "prime" is a prime of good reduction
 def is_prime_of_good_reduction(self,prime):
-    DS = copy(self)
-    degree = DS.degree_sequence(1)[0]
     try:
-        new_DS = DS.mod(prime)
-    except:
-        # the mod fucntion modDS throws exception only if
+        new_DS = self.mod(prime)
+    except ValueError:
+        # the mod fucntion modDS throws ValueError exception only if
         # the polynomials of the DynamicalSystem doesn't have the same degree which
         # means that this is a prime of bad reduction
-        return False
-    # check if the degree of the DynamicalSystem changed.
-    # yes -> prime of bad reduction
-    # no -> prime of good reduction
-    if new_DS.degree_sequence(1)[0] != degree :
         return False
     return True
--- mod_dynamical_system_prime.py
+++ mod_dynamical_system_prime.py
@@ -1,26 +1,23 @@
-# Function the returns the dynamical system mod p ( ds mod p )
+# %load mod_dynamical_system_prime.py
+# Function that return the dynamical system mod p ( ds mod p )
 def mod(self,p):
     DS = copy(self)
     DS.normalize_coordinates()  # normalize coordinates of dynamical system
     df_p_all = DS.defining_polynomials()
-    coefficients = []
-    for i in range(0,len(df_p_all)):
-        coefficients.append(df_p_all[i].coefficients())
-    monomials = []
-    for i in range(0,len(df_p_all)):
-        monomials.append(df_p_all[i].monomials())
+    coefficients = [poly.coefficients() for poly in DS.defining_polynomials()]
+    monomials = [poly.monomials() for poly in DS.defining_polynomials()]
     F = DS.base_ring() # obtain base_ring of dynamical system
     O = F.maximal_order()
     p = O.ideal(p).place()
     k, fr_k, to_k = p.residue_field()
     # reduce each coefficient of each polynomial using the function to_k :
     reduced_coefficients = []
-    for i in range(0,len(coefficients)):
-        reduced_coefficients.append([to_k(c) for c in coefficients[i]])
+    for coeff in coefficients:
+        reduced_coefficients.append([to_k(c) for c in coeff])
     # change the ring of each monomial to the residue field :
     new_monomials = []
-    for i in range(0,len(monomials)):
-        new_monomials.append([m.change_ring(k) for m in monomials[i]])
+    for monom in monomials:
+        new_monomials.append([m.change_ring(k) for m in monom])
     new_pol = []
     polynom = 0
     for i in range(0,len(reduced_coefficients)):
