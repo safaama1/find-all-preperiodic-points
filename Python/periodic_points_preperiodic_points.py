b5434993e521e7686e07208460cbe0c593e275d1
Safa Amasha <safaamasha99@gmail.com>
2021-06-04 15:53:40 +0300

    formatting files

--- Python/periodic_points_preperiodic_points.py
+++ Python/periodic_points_preperiodic_points.py
@@ -1,21 +1,42 @@
-def _all_periodic_points_(self,periods):
+def _all_periodic_points_(self, periods):
     DS = copy(self)
     # first find there is numbers that's dividible by another numbers
     # and keep the max between them
     # Example : periods = (3,5,15) => lst = (15)
     lst = []
-    periods  = sorted(periods)
-    for i in range(0,len(periods)) :
+    periods = sorted(periods)
+    for i in range(0, len(periods)):
         # put all the numbers that are divisible by periods[i]
-        f = list(reversed(list(filter(lambda x: (x % periods[i] == 0),periods))))
-        lst.append(f[0]) # keep the maximum
+        f = list(
+            reversed(list(filter(lambda x: (x % periods[i] == 0), periods))))
+        lst.append(f[0])  # keep the maximum
         lst = list(sorted((set(lst))))
     # now for every period find the periodic points and add them to the list
     periodic_points_lst = []
-    for period in lst :
-        periodic_points_lst += DS.periodic_points(period ,minimal = True)
+    for period in lst:
+        periodic_points_lst += DS.periodic_points(period, minimal=True)
         # 'minimal = True' because we removed the period = 1
     return list(set(periodic_points_lst))
+
+# edit to all_rational_preimages
+
+
+def all_rational_preimages(self, points):
+    from sage.rings.function_field.function_field import RationalFunctionField_global
+    if not isinstance(self.base_ring(), RationalFunctionField_global) and self.domain().base_ring() not in NumberFields():
+        raise TypeError("field won't return finite list of elements")
+    if not isinstance(points, (list, tuple)):
+        points = [points]
+    preperiodic = set()
+    while points != []:
+        P = points.pop()
+        preimages = self.rational_preimages(P)
+        for i in range(len(preimages)):
+            if not preimages[i] in preperiodic:
+                points.append(preimages[i])
+                preperiodic.add(preimages[i])
+    return list(preperiodic)
+
 
 def _all_preperiodic_points_(self, periodic_points):
     DS = copy(self)
--- Python/rational_preperiodic_graph.py
+++ Python/rational_preperiodic_graph.py
@@ -3,7 +3,7 @@ def rational_preperiodic_graph(self,primes,e):
     periods = DS.all_possible_global_periods(primes,e)
     periodic_points_ = DS._all_periodic_points_(periods)
     preperiodic_points_ = DS._all_preperiodic_points_(periodic_points_)
-    g1 = DS._preperiodic_points_to_cyclegraph(preperiodic_point_)
+    g1 = DS._preperiodic_points_to_cyclegraph(preperiodic_points_)
     # number every vertice on the graph
     g2 = g1.canonical_label()
     g3 = g2.copy(immutable=True)
